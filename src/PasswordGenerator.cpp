28a6a5bac4ed8bdc00da69e52547a4aee8477454
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-30 00:05:22 +0100

    Construct Function

---
+++ src/PasswordGenerator.cpp
@@ -0,0 +1,13 @@
+// src/PasswordGenerator.cpp
+#include "PasswordGenerator.h"
+#include <random>
+
+PasswordGenerator::PasswordGenerator() {
+
+}
+
+std::string PasswordGenerator::generate(int length) {
+    std::string password;
+    // For the password generate
+    return password;
+}
