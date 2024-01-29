75a3f70f837f9baa4c852a6e5c5737ea858ebc5c
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-30 00:10:45 +0100

    generate the password (character) on PasswordGenrator.

--- src/PasswordGenerator.cpp
+++ src/PasswordGenerator.cpp
@@ -2,12 +2,19 @@
 #include "PasswordGenerator.h"
 #include <random>
 
-PasswordGenerator::PasswordGenerator() {
-
+PasswordGenerator::PasswordGenerator() : characterPool() {
 }
 
 std::string PasswordGenerator::generate(int length) {
     std::string password;
-    // For the password generate
+    std::vector<char> characters = characterPool.getCharacters();
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_int_distribution<> distribution(0, characters.size() - 1);
+
+    for (int i = 0; i < length; ++i) {
+        password += characters[distribution(generator)];
+    }
+
     return password;
 }
