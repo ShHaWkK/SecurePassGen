dc5faffe83d983a34df8539db4971b23fa88eed3
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-30 00:11:56 +0100

    Update characterPool.cpp (character initialisation)

--- src/CharacterPool.cpp
+++ src/CharacterPool.cpp
@@ -6,6 +6,24 @@
 
 
 CharacterPool::CharacterPool() {
+    // Initialisation avec un ensemble standard de caractères
+    for (char c = '0'; c <= '9'; ++c) characters.push_back(c);
+    for (char c = 'A'; c <= 'Z'; ++c) characters.push_back(c);
+    for (char c = 'a'; c <= 'z'; ++c) characters.push_back(c);
+    characters.push_back('!');
+    characters.push_back('@');
+    characters.push_back('#');
+    characters.push_back('$');
+    characters.push_back('%');
+    characters.push_back('^');
+    characters.push_back('&');
+    characters.push_back('*');
+    characters.push_back('(');
+    characters.push_back(')');
+}
+
+std::vector<char> CharacterPool::getCharacters() const {
+    return characters;
 }
 
 std::vector<char> CharacterPool::getCharacters() const {
