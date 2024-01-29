7e55b602a968e33b364250208ca747b61079a5fc
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-30 00:07:08 +0100

    Update CharacterPool.h

--- src/CharacterPool.h
+++ src/CharacterPool.h
@@ -1,14 +1,20 @@
 //
-// Created by alexa on 29/01/2024.
+// Created by ShHawk on 29/01/2024.
 //
 
-#ifndef SECUREPASSGEN_CHARACTERPOOL_H
-#define SECUREPASSGEN_CHARACTERPOOL_H
+#ifndef CHARACTERPOOL_H
+#define CHARACTERPOOL_H
 
+#include <string>
+#include <vector>
 
 class CharacterPool {
-
+public:
+    CharacterPool();
+    std::vector<char> getCharacters() const;
+private:
+    std::vector<char> characters;
 };
 
+#endif //CHARACTERPOOL_H
 
-#endif //SECUREPASSGEN_CHARACTERPOOL_H
