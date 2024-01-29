c488c5b390df770047e08a31281fc187e99ffd17
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-30 00:02:45 +0100

    ADD PasswordGenerator.h

--- src/PasswordGenerator.h
+++ src/PasswordGenerator.h
@@ -2,13 +2,20 @@
 // Created by ShHawk on 29/01/2024.
 //
 
-#ifndef SECUREPASSGEN_PASSWORDGENERATOR_H
-#define SECUREPASSGEN_PASSWORDGENERATOR_H
 
+#ifndef PASSWORDGENERATOR_H
+#define PASSWORDGENERATOR_H
+
+#include <string>
+#include "CharacterPool.h"
 
 class PasswordGenerator {
-
+public:
+    PasswordGenerator();
+    std::string generate(int length);
+private:
+    CharacterPool characterPool;
 };
 
+#endif //PASSWORDGENERATOR_H
 
-#endif //SECUREPASSGEN_PASSWORDGENERATOR_H
