bbcfd17c25c206d5bd667ba655d9ed7ca51b19b3
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-30 00:03:27 +0100

    Update main.cpp (merge PasswordGenerate.h)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
+// src/main.cpp
+#include "PasswordGenerator.h"
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int length;
+    std::cout << "Enter the length of the password: ";
+    std::cin >> length;
+
+    PasswordGenerator passwordGenerator;
+    std::string password = passwordGenerator.generate(length);
+    std::cout << "Generated Password: " << password << std::endl;
+
     return 0;
 }
