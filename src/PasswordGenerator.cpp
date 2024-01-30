6cb76b7c886da482db5f650564afcaf37234e4e6
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-30 11:14:05 +0100

    Update PasswordGenerator.cpp

--- src/PasswordGenerator.cpp
+++ src/PasswordGenerator.cpp
@@ -1,8 +1,6 @@
 // src/PasswordGenerator.cpp
 #include "PasswordGenerator.h"
-#include <algorithm>
-#include <iterator>
-#include <vector>
+
 
 PasswordGenerator::PasswordGenerator() : gen(rd()) {
     // Configuration initiale si nécessaire
@@ -17,28 +15,25 @@ std::string PasswordGenerator::generate(int length, int minLetters, int minDigit
     std::string password;
     password.reserve(length);
 
-    // Ajouter le nombre minimum requis de chaque type de caractère
     for (int i = 0; i < minLetters; ++i) password.push_back(getRandomChar(characterPool.getLetters()));
     for (int i = 0; i < minDigits; ++i) password.push_back(getRandomChar(characterPool.getDigits()));
     for (int i = 0; i < minSymbols; ++i) password.push_back(getRandomChar(characterPool.getSymbols()));
 
-    // Remplir le reste du mot de passe avec des caractères aléatoires de tous types
     while (password.size() < length) {
         password.push_back(getRandomChar(characterPool.getLetters()));
         if (password.size() < length) password.push_back(getRandomChar(characterPool.getDigits()));
         if (password.size() < length) password.push_back(getRandomChar(characterPool.getSymbols()));
     }
 
-    // Mélanger le mot de passe pour éviter les modèles prévisibles
     std::shuffle(password.begin(), password.end(), gen);
 
-    std::vector<std::string> PasswordGenerator::generateMultiple(int numberOfPasswords, int length, int minLetters, int minDigits, int minSymbols)
-    {
-        std::vector<std::string> passwords;
-        for (int i = 0; i < numberOfPasswords; ++i) {
-            passwords.push_back(generate(length, minLetters, minDigits, minSymbols));
-        }
-        return passwords;
+    return password;
+}
+
+std::vector<std::string> PasswordGenerator::generateMultiple(int numberOfPasswords, int length, int minLetters, int minDigits, int minSymbols) {
+    std::vector<std::string> passwords;
+    for (int i = 0; i < numberOfPasswords; ++i) {
+        passwords.push_back(generate(length, minLetters, minDigits, minSymbols));
     }
-    
+    return passwords;
 }
