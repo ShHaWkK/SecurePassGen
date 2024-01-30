e12dde364f5e83570f6a6c9b694326840d1e0609
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-30 11:26:00 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,34 @@
-// src/main.cpp
+#include "CharacterPool.h"
 #include "PasswordGenerator.h"
 #include <iostream>
 
 int main() {
-    int length;
-    std::cout << "Enter the length of the password: ";
-    std::cin >> length;
-
     PasswordGenerator passwordGenerator;
-    std::string password = passwordGenerator.generate(length);
-    std::cout << "Generated Password: " << password << std::endl;
+    int numberOfPasswords, length, minLetters, minDigits, minSymbols;
+
+    std::cout << "Enter the number of passwords to generate: ";
+    std::cin >> numberOfPasswords;
+    std::cout << "Enter the length of each password: ";
+    std::cin >> length;
+    // Demandez minLetters, minDigits, minSymbols de la même manière
+
+    auto passwords = passwordGenerator.generateMultiple(numberOfPasswords, length, minLetters, minDigits, minSymbols);
+
+    // Optionnellement, demandez si l'utilisateur veut sauvegarder les mots de passe
+    std::string saveOption;
+    std::cout << "Do you want to save the passwords to a file? (yes/no): ";
+    std::cin >> saveOption;
+    if (saveOption == "yes") {
+        std::string filename;
+        std::cout << "Enter the filename to save passwords: ";
+        std::cin >> filename;
+        passwordGenerator.savePasswordsToFile(passwords, filename);
+    }
+
+    // Affichez les mots de passe générés
+    for (const auto& password : passwords) {
+        std::cout << password << std::endl;
+    }
 
     return 0;
 }
