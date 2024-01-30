ca441c971bfd062246ecd28b50307dce26fe80b0
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-30 10:54:11 +0100

    Update PasswordGenrator.h (update)

--- src/PasswordGenerator.h
+++ src/PasswordGenerator.h
@@ -12,10 +12,15 @@
 class PasswordGenerator {
 public:
     PasswordGenerator();
-    std::string generate(int length);
+    std::string generate(int length, int minLetters = 0, int minDigits = 0, int minSymbols = 0);
+
 private:
     CharacterPool characterPool;
+    std::random_device rd;
+    std::mt19937 gen;
+    char getRandomChar(const std::vector<char>& chars);
 };
+
 
 #endif //PASSWORDGENERATOR_H
 
